65b996d3d9053476d81f6946f3a0108c720e68b8
Jaakko Tulkki <jaakko.tulkki@gmail.com>
2015-06-08 22:46:44 +0200

    improved Deck and added multiplaying

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # poker-game
 
 This poker game deals a hand of five cards and evaluates ranks and combinations of those cards. Good for evaluating your chanches of getting different combinations on poker.
+
+Multiplayer game is also included, the deck is a singleton using borg design pattern.
+
--- poker.py
+++ poker.py
@@ -1,7 +1,7 @@
 import random
 SUITS = ['S', 'D', 'H', 'C']
 VALUES = ["2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"]
-class Card:
+class Card(object):
     def __init__(self, card):
         card = card.split(" ")
         self.suit = card[0]
@@ -19,10 +19,21 @@ class Card:
     def __lt__(self, other):
         return self.value < other.value
 
+class BaseDeck(object):
+    _shared_state = {}
 
-class Deck:
+    def __new__(cls, *args, **kwargs):
+        obj = super(BaseDeck, cls).__new__(cls)
+        obj.__dict__ = cls._shared_state
+        return obj
+
+class Deck(BaseDeck):
+    pass
+
+class Poker(object):
     def __init__(self):
-        self.deck = Deck.create_deck()
+        if not hasattr(Deck, "deck"):
+            Deck.deck = Poker.create_deck()
         self.shuffle()
 
     @staticmethod
@@ -34,19 +45,15 @@ class Deck:
         return deck
 
     def shuffle(self):
-        random.shuffle(self.deck)
+        random.shuffle(Deck.deck)
 
     def give_cards(self, num):
-        cards = self.deck[:num]
-        self.deck = self.deck[num:]
-        return cards
-
-class Poker:
-    def __init__(self):
-        self.deck = Deck()
+        five_cards = Deck.deck[:num]
+        Deck.deck = Deck.deck[num:]
+        return five_cards
 
     def give_hand(self):
-        self.hand = self.deck.give_cards(5)
+        self.hand = self.give_cards(5)
         self.values = [e.value for e in self.hand]
 
     def evaluate_hand(self):
@@ -61,7 +68,7 @@ class Poker:
         self.roayl_flush = self.has_roayl_flush()
 
     def set_high_card(self):
-        return max(self.hand, key=lambda x: VALUES.index(x.value))
+        return [max(self.hand, key=lambda x: VALUES.index(x.value))]
 
     def has_pairs(self):
         if len(set(self.values)) != 4:
@@ -103,8 +110,17 @@ class Poker:
             return False
         if max(values) - min(values) == 4:
             return self.hand
-        else:
+        #if we don't have ACE in the hand
+        elif 13 not in values:
             return False
+        #if we have it
+        else:
+            values.remove(max)
+            values.append(-1)
+            if max(values) - min(values) == 4:
+                return self.hand
+            else:
+                return False
 
     def has_flush(self):
         return True if len(set([e.suit for e in self.hand])) == 1 else False
@@ -135,7 +151,7 @@ class Poker:
 
     def return_best(self):
         combos = [self.has_royal_flush, self.has_four_of_kind, self.has_full_house, self.has_straight,
-         self.has_threes, self.has_two_pairs, self.has_pairs, self.set_high_card]
+                  self.has_threes, self.has_two_pairs, self.has_pairs, self.set_high_card]
         for e in enumerate(combos):
             res = combos[e[0]]()
             if res:
@@ -145,7 +161,7 @@ if __name__ == "__main__":
     import time
     start = time.time()
     results = {}
-    for e in range(100):
+    for e in range(1000):
         p = Poker()
         p.give_hand()
         best, cards = p.return_best()
@@ -153,5 +169,32 @@ if __name__ == "__main__":
             results[best] = 1
         else:
             results[best] += 1
+        del Deck.deck
     print(results)
     print(time.time() - start)
+
+    #let's start playing
+    results = {}
+    start = time.time()
+    for e in range(10000):
+        player1 = Poker()
+        player1.give_hand()
+        player1_best, player1_cards = player1.return_best()
+
+        player2 = Poker()
+        player2.give_hand()
+        player2_best, player2_cards = player2.return_best()
+
+        assert (len(Deck.deck) == 42)
+
+        result = "Player 1 wins" if player1_best < player2_best else "Player 2 wins" if player2_best < player1_best else "It's a tie"
+        if result not in results:
+            results[result] = 1
+        else:
+            results[result] += 1
+        del Deck.deck
+    print(results)
+    print("End time for two player games ", time.time() - start)
+
+
+
